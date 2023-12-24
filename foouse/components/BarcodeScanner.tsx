aaf00b0d0670d6a7c12d20049935596be5d00819
Ignasi Cervero <ignasicervero99@gmail.com>
2023-12-24 16:57:50 +0100

    Add some safe checks BarcodeScanner

--- foouse/components/BarcodeScanner.tsx
+++ foouse/components/BarcodeScanner.tsx
@@ -2,8 +2,10 @@ import React from "react";
 import { Text, StyleSheet } from "react-native";
 import { Camera, CodeScanner, CameraCaptureError } from "react-native-vision-camera";
 import { router } from 'expo-router';
+import { useIsFocused } from "@react-navigation/native";
 
 const BarcodeScanner = () => {
+  const isFocused = useIsFocused();
 
   const devices = Camera.getAvailableCameraDevices();
   const device = devices.find((d) => d.position === 'back');
@@ -11,6 +13,14 @@ const BarcodeScanner = () => {
   const codeScanner: CodeScanner = {
     codeTypes: ['ean-13'],
     onCodeScanned: (codes) => {
+
+      if (!codes) {
+        return;
+      }
+
+      if (codes.length === 0) {
+        return;
+      }
 
       if (!codes[0].value) {
         return;
@@ -24,7 +34,7 @@ const BarcodeScanner = () => {
   return (
     <Camera
       device={device}
-      isActive={true}
+      isActive={isFocused}
       style={StyleSheet.absoluteFill}
       codeScanner={codeScanner}
     />
