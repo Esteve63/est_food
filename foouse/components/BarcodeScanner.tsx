daf286dbe09237312b29e509d83c1a8b8bdb331b
Ignasi Cervero <ignasicervero99@gmail.com>
2023-12-25 12:01:42 +0100

    WIP: add camera focus on tab

--- foouse/components/BarcodeScanner.tsx
+++ foouse/components/BarcodeScanner.tsx
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Text, StyleSheet } from "react-native";
-import { Camera, CodeScanner, CameraCaptureError } from "react-native-vision-camera";
+import { Camera, CodeScanner } from "react-native-vision-camera";
+import { GestureDetector, Gesture } from "react-native-gesture-handler";
 import { router } from 'expo-router';
 import { useIsFocused } from "@react-navigation/native";
 
 const BarcodeScanner = () => {
   const isFocused = useIsFocused();
+  const cameraRef = useRef<any>(null);
 
   const devices = Camera.getAvailableCameraDevices();
   const device = devices.find((d) => d.position === 'back');
+
+  if (device == null) {
+    return (<Text>NO CAMERA DEVICE</Text>);
+  }
 
   const codeScanner: CodeScanner = {
     codeTypes: ['ean-13'],
@@ -30,14 +36,29 @@ const BarcodeScanner = () => {
     }
   }
 
-  if (device == null) return (<Text>NO CAMERA DEVICE</Text>)
+  const tap = Gesture.Tap()
+    .onStart(async ({x, y}) => {
+      console.log(x, y);
+      cameraRef.current.focus({x, y})
+      // if (cameraRef.current && device.supportsFocus) {
+      //   try {
+      //     await cameraRef.current.focus({ x, y });
+      //   } catch (error) {
+      //     console.error("Error focusing:", error);
+      //   }
+      // }
+    });
+
   return (
-    <Camera
-      device={device}
-      isActive={isFocused}
-      style={StyleSheet.absoluteFill}
-      codeScanner={codeScanner}
-    />
+    <GestureDetector gesture={tap}>
+      <Camera
+        ref={cameraRef}
+        device={device}
+        isActive={isFocused}
+        style={StyleSheet.absoluteFill}
+        codeScanner={codeScanner}
+      />
+    </GestureDetector>
   );
 };
 
