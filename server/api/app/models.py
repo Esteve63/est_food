703dc058023303bf3546df7fb0ee396c1b62be75
Ignasi Cervero <ignasicervero99@gmail.com>
2023-12-31 16:11:10 +0100

    Delete compound pkey

--- server/api/app/models.py
+++ server/api/app/models.py
@@ -9,14 +9,15 @@ class Warehouse(SQLModel, table=True):
     name: str = Field(max_length=20)
 
 class Category(SQLModel, table=True):
-    warehouse_id: int = Field(primary_key=True, foreign_key="warehouse.id")
-    name: str = Field(primary_key=True)
+    id: tp.Optional[int] = Field(default_factory=None, primary_key=True)
+    warehouse_id: int = Field(foreign_key="warehouse.id")
+    name: str = Field()
     min_stock: int = Field(default=0)
 
 class Product(SQLModel, table=True):
-    ean_code: str = Field(default=None, primary_key=True, max_length=13)
-    warehouse_id: int = Field() # foreign_key="category.warehouse_id"
-    category_name: str = Field() # foreign_key="category.name"
+    id: tp.Optional[int] = Field(default_factory=None, primary_key=True)
+    ean_code: str = Field(default=None, max_length=13)
+    category_id: int = Field(foreign_key="category.id")
     value: int = 0
     units: str = ''
     stock: int = 0
